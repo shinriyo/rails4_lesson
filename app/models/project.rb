c0e7a62be57cde11c260d9bacf91bcb6b475b860
shinriyo <shinriyo@gmail.comm>
2013-10-31 23:09:29 +0900

    finish

---
+++ app/models/project.rb
@@ -0,0 +1,8 @@
+# -*- encoding: utf-8 -*-
+class Project < ActiveRecord::Base
+  has_many :tasks
+  #validates :title, presence: true
+  validates :title,
+  presence: { message: "入力してください" },
+  length: { minimum: 3, message: "短すぎ！" }
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Taskapp::Application.config.secret_key_base = 'a619723dca4cc6f4a91ddd69c78e77c64e123f3a87fe890a9791505b2fd61e468d167243ee2f9cfedce81b73a55d90657eb085efa89ba9149b86fb9f182f7933'
